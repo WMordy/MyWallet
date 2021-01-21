d94fc3aad2529ccafde49098ef552be22e0524d1
wmordy <mwadii30@gmail.com>
2021-01-21 10:50:28 +0100

    adding server methods

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,17 +1,38 @@
 var express = require("express")
 var app  = express()
 
+
+app.use(express.json()); 
+
 app.get("/", (req,res)=>(
     res.send("hello world")
 ))
 
 app.post("/auth",(req,res)=>{
+    let username  = req.body.username 
+    let hashedPass = req.body.hashedPass
+    console.log(username+"    "+hashedPass);
+    console.log(req.body)
+    res.sendStatus(200)
     //TODO setup authentification here
+
 })
 
-app.get("/userCoordinations",(req,res)=>{
+app.get("/userCoordinations/:username",(req,res)=>{
+    let username = req.params.username
+    console.log(username)
     //TODO setup users infos here 
-    res.send("coordinates")
+    let Coordinates = {
+        "ig" : "www.server.com",
+        "facebook" : "fb.com/server"
+    }
+    res.json(Coordinates)
 })
 
-app.listen(5000);
+app.get("/checkUser/:user",(req,res)=>{
+    let username = req.params.user
+    console.log(username)
+    res.sendStatus(200)
+})
+
+app.listen(2699);
