0c0fd75f0c91d96e3295acc58fda7aef607198cd
wmordy <mwadii30@gmail.com>
2021-01-30 15:27:10 +0100

    setup endpoint coordinates with database

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,17 +1,28 @@
+const { response } = require("express")
 var express = require("express")
 var app  = express()
-
+var mongoose = require("mongoose")
 var CoordinatesArray = [
                                ["Instagram" , "www.server.com"],
                                ["Facebook" ,"fb.com/server"]]
 var UsersArray = [
                                ["test" , "4Bjx5WkqDQFVwdc1lUA4Yw==\n"],
                                ["sammu" ,"/puJxSckU46acuBrVHYWKw==\n"]]
+
 app.use(express.json()); 
 
 app.get("/", (req,res)=>(
     res.send("hello world")
 ))
+
+
+var CoordinateSchema = mongoose.Schema({
+    username : String,
+    type : String ,
+    value : String 
+})
+var Coordinate = mongoose.model("Coordinate",CoordinateSchema)
+
 
 app.post("/auth",(req,res)=>{
     let username  = req.body.username 
@@ -30,6 +41,14 @@ app.post("/addCoordination",(req,res)=>{
     let username = req.body.username
     let type  = req.body.type 
     let value = req.body.value
+    mongoose.connect('mongodb://localhost/MyWallet');
+    let newCoordinate = new Coordinate({
+        username : username,
+    type : type ,
+    value : value 
+    })
+    newCoordinate.save();
+    
     CoordinatesArray.push([type,value])
     console.log(CoordinatesArray);
     console.log(req.body)
@@ -45,6 +64,9 @@ app.get("/userCoordinations/:username",(req,res)=>{
     let username = req.params.username
     console.log("sending coordinations")
     //TODO setup users infos here 
+    Coordinate.find({username : username},(err,response) => {
+        console.log(response)
+    })
     let Coordinates = {
        "data" : CoordinatesArray
     }
